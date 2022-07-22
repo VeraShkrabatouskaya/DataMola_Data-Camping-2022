219e38c5d1b06285add12e2e3d14820309418b47
VeraShkrabatouskaya <98982851+VeraShkrabatouskaya@users.noreply.github.com>
2022-07-23 01:56:39 +0300

    Add files via upload

---
+++ U1M7.LW.Dimension and Facts Basics Part 1/scripts_task01/u_dw_references/Tables/t_quarters/sq_quarters.sql
@@ -0,0 +1,7 @@
+--drop sequence u_dw_references.sq_quarter_id;
+
+alter session set current_schema=u_dw_references;
+
+create sequence u_dw_references.sq_quarter_id start with 1;
+
+grant SELECT on u_dw_references.sq_quarter_id to u_dw_ext_references;
