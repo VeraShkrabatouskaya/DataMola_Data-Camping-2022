d63140292df8034fecbf2dbaadba0bb3c8420854
VeraShkrabatouskaya <98982851+VeraShkrabatouskaya@users.noreply.github.com>
2022-07-23 01:58:46 +0300

    Add files via upload

---
+++ U1M7.LW.Dimension and Facts Basics Part 1/scripts_task01/u_dw_references/t_years/t_years_init.sql
@@ -0,0 +1,16 @@
+alter session set current_schema=u_dw_references;
+
+INSERT INTO t_years ( 
+    DAYS_IN_CAL_YEAR,     
+    BEG_OF_CAL_YEAR,      
+    END_OF_CAL_YEAR,
+    CALENDAR_YEAR
+ )
+  SELECT DAYS_IN_CAL_YEAR,     
+    BEG_OF_CAL_YEAR,      
+    END_OF_CAL_YEAR,
+    CALENDAR_YEAR FROM u_dw_ext_references.cls_years;
+     
+COMMIT;
+
+SELECT * FROM t_years;
