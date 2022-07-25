3ffad480295fd02d20c770485774437e81f7fc08
VeraShkrabatouskaya <98982851+VeraShkrabatouskaya@users.noreply.github.com>
2022-07-25 09:18:08 +0300

    Create t_address_types.sql

---
+++ U1M6.LW.Star Schema Basics Part 1/scripts_task02/u_dw_references/tables/t_address_types/t_address_types.sql
@@ -0,0 +1,21 @@
+--drop table u_dw_references.t_address_types cascade constraints;
+
+/*==============================================================*/
+/* Table: t_address_types                                     */
+
+/*==============================================================*/
+alter session set current_schema=u_dw_references;
+alter user u_dw_references quota unlimited on TS_REFERENCES_DATA_01;
+
+create table u_dw_references.t_address_types 
+(
+   adress_type_id     NUMBER(22,0)         not null,
+   adress_type_code   VARCHAR2(30 CHAR)    not null,
+   adress_type_desc   VARCHAR2(200 CHAR)   not null,
+   constraint PK_T_ADDRESS_TYPES primary key (adress_type_id)
+         using index tablespace TS_REFERENCES_IDX_01
+)
+tablespace TS_REFERENCES_DATA_01;
+
+alter table u_dw_references.t_address_types
+   add constraint CKC_ADRESS_TYPE_CODE_T_ADDRES check (adress_type_code = upper(adress_type_code));
