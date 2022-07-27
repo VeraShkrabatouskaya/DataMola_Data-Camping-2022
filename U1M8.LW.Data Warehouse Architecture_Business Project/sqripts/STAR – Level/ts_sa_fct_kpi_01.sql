93170797cee0d733dda03ab6fe78025e114ce3bb
VeraShkrabatouskaya <98982851+VeraShkrabatouskaya@users.noreply.github.com>
2022-07-27 23:06:05 +0300

    Add files via upload

---
+++ U1M8.LW.Data Warehouse Architecture_Business Project/sqripts/STAR – Level/ts_sa_fct_kpi_01.sql
@@ -0,0 +1,17 @@
+--DROP TABLESPACE ts_sa_fct_kpi_01 INCLUDING CONTENTS AND DATAFILES CASCADE CONSTRAINTS;
+--DROP USER dm_kpi;
+
+--SELECT * from dba_data_files ;
+--ALTER SYSTEM SET db_create_file_dest = '/oracle/u02/oradata/VShkrabatovskayadb';
+ 
+CREATE TABLESPACE ts_sa_fct_kpi_01
+DATAFILE '/oracle/u02/oradata/VShkrabatovskayadb/db_ts_sa_fct_kpi_01.dat'
+SIZE 150M
+AUTOEXTEND ON NEXT 50M
+SEGMENT SPACE MANAGEMENT AUTO; 
+
+CREATE USER dm_kpi
+  IDENTIFIED BY "%PWD%"
+    DEFAULT TABLESPACE ts_sa_fct_kpi_01;
+
+GRANT CONNECT,RESOURCE, CREATE VIEW TO dm_kpi;
