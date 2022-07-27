dd0a8df692c7c793800352a72bb7f284f208bd21
VeraShkrabatouskaya <98982851+VeraShkrabatouskaya@users.noreply.github.com>
2022-07-27 23:07:24 +0300

    Add files via upload

---
+++ U1M8.LW.Data Warehouse Architecture_Business Project/sqripts/Storage level SA/ts_sa_employees_data_01.sql
@@ -0,0 +1,17 @@
+--DROP TABLESPACE ts_sa_employees_data_01 INCLUDING CONTENTS AND DATAFILES CASCADE CONSTRAINTS;
+--DROP USER sa_employees;
+
+--SELECT * from dba_data_files ;
+--ALTER SYSTEM SET db_create_file_dest = '/oracle/u02/oradata/VShkrabatovskayadb';
+
+CREATE TABLESPACE ts_sa_employees_data_01
+DATAFILE '/oracle/u02/oradata/VShkrabatovskayadb/db_ts_sa_employees_data_01.dat'
+SIZE 150M
+ AUTOEXTEND ON NEXT 50M
+ SEGMENT SPACE MANAGEMENT AUTO; 
+
+CREATE USER sa_employees
+  IDENTIFIED BY "%PWD%"
+    DEFAULT TABLESPACE ts_sa_employees_data_01;
+
+GRANT CONNECT,RESOURCE TO sa_employees;
