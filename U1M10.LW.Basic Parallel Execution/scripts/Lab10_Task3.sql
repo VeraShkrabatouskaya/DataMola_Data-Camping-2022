4c983c02a028bbe104bc2115f8c25ca5370d2da2
VeraShkrabatouskaya <98982851+VeraShkrabatouskaya@users.noreply.github.com>
2022-07-29 20:09:34 +0300

    Add files via upload

---
+++ U1M10.LW.Basic Parallel Execution/scripts/Lab10_Task3.sql
@@ -0,0 +1,153 @@
+SELECT * from dba_data_files ;
+select * from USER_tablespaces;
+
+CREATE TABLESPACE ts_dw_data_02
+DATAFILE '/oracle/u02/oradata/VShkrabatovskayadb/db_qpt_dw_data_02.dat'
+SIZE 200M
+ AUTOEXTEND ON NEXT 100M
+ SEGMENT SPACE MANAGEMENT AUTO;
+
+CREATE USER u_dw_data_02
+  IDENTIFIED BY "%PWD%"
+    DEFAULT TABLESPACE ts_dw_data_02;
+
+GRANT CONNECT,RESOURCE TO u_dw_data_02;
+
+ALTER USER u_dw_data_02 quota unlimited on ts_dw_data_02;
+alter session set current_schema=u_dw_data_02;
+--------------------------------------------------------------------------------
+
+--drop table calendar;
+
+alter session set nls_territory = "UNITED KINGDOM";
+
+explain plan for
+--SET TIMING ON;
+CREATE TABLE calendar
+AS SELECT * FROM ( 
+SELECT 
+  TRUNC( sd + rn ) time_id,
+  TO_CHAR( sd + rn, 'fmDay' ) day_name,
+  TO_CHAR( sd + rn, 'D' ) day_number_in_week,
+  TO_CHAR( sd + rn, 'DD' ) day_number_in_month,
+  TO_CHAR( sd + rn, 'DDD' ) day_number_in_year,
+  TO_CHAR( sd + rn, 'W' ) calendar_week_number,
+  ( CASE
+      WHEN TO_CHAR( sd + rn, 'D' ) IN ( 1, 2, 3, 4, 5, 6 ) THEN
+        NEXT_DAY( sd + rn, 'SUNDAY' )
+      ELSE
+        ( sd + rn )
+    END ) week_ending_date,
+  TO_CHAR( sd + rn, 'MM' ) calendar_month_number,
+  TO_CHAR( LAST_DAY( sd + rn ), 'DD' ) days_in_cal_month,
+  LAST_DAY( sd + rn ) end_of_cal_month,
+  TO_CHAR( sd + rn, 'FMMonth' ) calendar_month_name,
+  ( ( CASE
+      WHEN TO_CHAR( sd + rn, 'Q' ) = 1 THEN
+        TO_DATE( '03/31/' || TO_CHAR( sd + rn, 'YYYY' ), 'MM/DD/YYYY' )
+      WHEN TO_CHAR( sd + rn, 'Q' ) = 2 THEN
+        TO_DATE( '06/30/' || TO_CHAR( sd + rn, 'YYYY' ), 'MM/DD/YYYY' )
+      WHEN TO_CHAR( sd + rn, 'Q' ) = 3 THEN
+        TO_DATE( '09/30/' || TO_CHAR( sd + rn, 'YYYY' ), 'MM/DD/YYYY' )
+      WHEN TO_CHAR( sd + rn, 'Q' ) = 4 THEN
+        TO_DATE( '12/31/' || TO_CHAR( sd + rn, 'YYYY' ), 'MM/DD/YYYY' )
+    END ) - TRUNC( sd + rn, 'Q' ) + 1 ) days_in_cal_quarter,
+  TRUNC( sd + rn, 'Q' ) beg_of_cal_quarter,
+  ( CASE
+      WHEN TO_CHAR( sd + rn, 'Q' ) = 1 THEN
+        TO_DATE( '03/31/' || TO_CHAR( sd + rn, 'YYYY' ), 'MM/DD/YYYY' )
+      WHEN TO_CHAR( sd + rn, 'Q' ) = 2 THEN
+        TO_DATE( '06/30/' || TO_CHAR( sd + rn, 'YYYY' ), 'MM/DD/YYYY' )
+      WHEN TO_CHAR( sd + rn, 'Q' ) = 3 THEN
+        TO_DATE( '09/30/' || TO_CHAR( sd + rn, 'YYYY' ), 'MM/DD/YYYY' )
+      WHEN TO_CHAR( sd + rn, 'Q' ) = 4 THEN
+        TO_DATE( '12/31/' || TO_CHAR( sd + rn, 'YYYY' ), 'MM/DD/YYYY' )
+    END ) end_of_cal_quarter,
+  TO_CHAR( sd + rn, 'Q' ) calendar_quarter_number,
+  TO_CHAR( sd + rn, 'YYYY' ) calendar_year,
+  ( TO_DATE( '12/31/' || TO_CHAR( sd + rn, 'YYYY' ), 'MM/DD/YYYY' )
+    - TRUNC( sd + rn, 'YEAR' ) ) days_in_cal_year,
+  TRUNC( sd + rn, 'YEAR' ) beg_of_cal_year,
+  TO_DATE( '12/31/' || TO_CHAR( sd + rn, 'YYYY' ), 'MM/DD/YYYY' ) end_of_cal_year
+FROM
+  ( 
+    SELECT 
+      TO_DATE( '12/31/2011', 'MM/DD/YYYY' ) sd,
+      rownum rn
+    FROM dual
+      CONNECT BY level <=10000
+  ));
+--SET TIMING OFF;
+
+select * from table (dbms_xplan.display);
+--------------------------------------------------------------------------------
+
+--drop table calendar;
+
+alter session set nls_territory = "UNITED KINGDOM";
+
+--explain plan for
+SET TIMING ON;
+CREATE TABLE calendar PARALLEL 4
+AS SELECT * FROM ( 
+SELECT 
+  TRUNC( sd + rn ) time_id,
+  TO_CHAR( sd + rn, 'fmDay' ) day_name,
+  TO_CHAR( sd + rn, 'D' ) day_number_in_week,
+  TO_CHAR( sd + rn, 'DD' ) day_number_in_month,
+  TO_CHAR( sd + rn, 'DDD' ) day_number_in_year,
+  TO_CHAR( sd + rn, 'W' ) calendar_week_number,
+  ( CASE
+      WHEN TO_CHAR( sd + rn, 'D' ) IN ( 1, 2, 3, 4, 5, 6 ) THEN
+        NEXT_DAY( sd + rn, 'SUNDAY' )
+      ELSE
+        ( sd + rn )
+    END ) week_ending_date,
+  TO_CHAR( sd + rn, 'MM' ) calendar_month_number,
+  TO_CHAR( LAST_DAY( sd + rn ), 'DD' ) days_in_cal_month,
+  LAST_DAY( sd + rn ) end_of_cal_month,
+  TO_CHAR( sd + rn, 'FMMonth' ) calendar_month_name,
+  ( ( CASE
+      WHEN TO_CHAR( sd + rn, 'Q' ) = 1 THEN
+        TO_DATE( '03/31/' || TO_CHAR( sd + rn, 'YYYY' ), 'MM/DD/YYYY' )
+      WHEN TO_CHAR( sd + rn, 'Q' ) = 2 THEN
+        TO_DATE( '06/30/' || TO_CHAR( sd + rn, 'YYYY' ), 'MM/DD/YYYY' )
+      WHEN TO_CHAR( sd + rn, 'Q' ) = 3 THEN
+        TO_DATE( '09/30/' || TO_CHAR( sd + rn, 'YYYY' ), 'MM/DD/YYYY' )
+      WHEN TO_CHAR( sd + rn, 'Q' ) = 4 THEN
+        TO_DATE( '12/31/' || TO_CHAR( sd + rn, 'YYYY' ), 'MM/DD/YYYY' )
+    END ) - TRUNC( sd + rn, 'Q' ) + 1 ) days_in_cal_quarter,
+  TRUNC( sd + rn, 'Q' ) beg_of_cal_quarter,
+  ( CASE
+      WHEN TO_CHAR( sd + rn, 'Q' ) = 1 THEN
+        TO_DATE( '03/31/' || TO_CHAR( sd + rn, 'YYYY' ), 'MM/DD/YYYY' )
+      WHEN TO_CHAR( sd + rn, 'Q' ) = 2 THEN
+        TO_DATE( '06/30/' || TO_CHAR( sd + rn, 'YYYY' ), 'MM/DD/YYYY' )
+      WHEN TO_CHAR( sd + rn, 'Q' ) = 3 THEN
+        TO_DATE( '09/30/' || TO_CHAR( sd + rn, 'YYYY' ), 'MM/DD/YYYY' )
+      WHEN TO_CHAR( sd + rn, 'Q' ) = 4 THEN
+        TO_DATE( '12/31/' || TO_CHAR( sd + rn, 'YYYY' ), 'MM/DD/YYYY' )
+    END ) end_of_cal_quarter,
+  TO_CHAR( sd + rn, 'Q' ) calendar_quarter_number,
+  TO_CHAR( sd + rn, 'YYYY' ) calendar_year,
+  ( TO_DATE( '12/31/' || TO_CHAR( sd + rn, 'YYYY' ), 'MM/DD/YYYY' )
+    - TRUNC( sd + rn, 'YEAR' ) ) days_in_cal_year,
+  TRUNC( sd + rn, 'YEAR' ) beg_of_cal_year,
+  TO_DATE( '12/31/' || TO_CHAR( sd + rn, 'YYYY' ), 'MM/DD/YYYY' ) end_of_cal_year
+FROM
+  ( 
+    SELECT 
+      TO_DATE( '12/31/2011', 'MM/DD/YYYY' ) sd,
+      rownum rn
+    FROM dual
+      CONNECT BY level <=10000
+  ));
+SET TIMING OFF;
+
+--select * from table (dbms_xplan.display);
+--------------------------------------------------------------------------------
+--select * from calendar;
+
+
+
+
