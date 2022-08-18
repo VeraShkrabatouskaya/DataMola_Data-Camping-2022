ea47a607c9e631c0d1c00f0b53c123df14a39053
VeraShkrabatouskaya <98982851+VeraShkrabatouskaya@users.noreply.github.com>
2022-08-18 11:06:50 +0300

    Add files via upload

---
+++ U2M8-9.LW.ETL Overview - Extraction_Transportation/scripts/tables/sq_t_FCT_business.sql
@@ -0,0 +1,8 @@
+--drop sequence DW_DATA.SQ_FCT_business;
+--alter session set current_schema = DW_DATA;
+
+CREATE SEQUENCE DW_DATA.SQ_FCT_business
+    START WITH        1    
+    INCREMENT BY      1
+    NOCACHE
+    NOCYCLE;
---
+++ U2M8-9.LW.ETL Overview - Extraction_Transportation/scripts/tables/t_FCT_business.sql
@@ -0,0 +1,91 @@
+alter session set current_schema = DW_DATA;
+--drop table FCT_business CASCADE CONSTRAINTS;
+
+CREATE TABLE FCT_business(
+    Business_Fact_ID NUMBER(10),   
+    TIME_ID                                DATE,
+    customer_ID                            NUMBER(10),
+    employee_ID                            NUMBER(10),
+    agency_ID                              NUMBER(10),
+    gen_period_ID                          NUMBER(10),
+    product_ID                             NUMBER(10),
+    promotion_ID                           NUMBER(10),
+    gross_profit_dollar_amount             DECIMAL (30,2),
+    net_profit_dollar_amount               DECIMAL (30,2),
+    gross_revenue_dollar_amount            DECIMAL (30,2),
+    net_revenue_dollar_amount              DECIMAL (30,2),
+    gross_cost_dollar_amount               DECIMAL (30,2),
+    net_cost_dollar_amount                 DECIMAL (30,2),
+    gross_salary_employee_dollar_amount    DECIMAL (30,2),
+    net_salary_employee_dollar_amount      DECIMAL (30,2),
+    gross_profit_margin_percent            DECIMAL (10,2),
+    CONSTRAINT "PK_T.FCT_business" PRIMARY KEY(Business_Fact_ID)
+)
+
+PARTITION BY RANGE (TIME_ID)
+    subpartition by hash(agency_ID) subpartitions 4
+(
+    PARTITION QUARTER_1 VALUES LESS THAN(TO_DATE('01/04/2022', 'DD/MM/YYYY'))
+    ( subpartition QUARTER_1_sub_1,
+      subpartition QUARTER_1_sub_2,
+      subpartition QUARTER_1_sub_3,
+      subpartition QUARTER_1_sub_4
+    ),
+    PARTITION QUARTER_2 VALUES LESS THAN(TO_DATE('01/07/2022', 'DD/MM/YYYY'))
+    ( subpartition QUARTER_2_sub_1,
+      subpartition QUARTER_2_sub_2,
+      subpartition QUARTER_2_sub_3,
+      subpartition QUARTER_2_sub_4
+     ),
+     PARTITION QUARTER_3 VALUES LESS THAN(TO_DATE('01/10/2022', 'DD/MM/YYYY'))
+    ( subpartition QUARTER_3_sub_1,
+      subpartition QUARTER_3_sub_2,
+      subpartition QUARTER_3_sub_3,
+      subpartition QUARTER_3_sub_4
+    ),
+     PARTITION QUARTER_4 VALUES LESS THAN(TO_DATE('01/01/2023', 'DD/MM/YYYY'))
+    ( subpartition QUARTER_4_sub_1,
+      subpartition QUARTER_4_sub_2,
+      subpartition QUARTER_4_sub_3,
+      subpartition QUARTER_4_sub_4
+    )
+);
+   
+ALTER TABLE FCT_business
+   ADD CONSTRAINT "FK_FCT_business_DIM_customer" FOREIGN KEY (customer_ID )REFERENCES dim_customer (customer_ID);
+   
+ALTER TABLE FCT_business
+   ADD CONSTRAINT "FK_FCT_business_DIM_employee" FOREIGN KEY (employee_ID )REFERENCES dim_employee (employee_ID);
+   
+ALTER TABLE FCT_business
+   ADD CONSTRAINT "FK_FCT_business_DIM_agency" FOREIGN KEY (agency_ID )REFERENCES dim_agency (agency_ID);
+   
+ALTER TABLE FCT_business
+   ADD CONSTRAINT "FK_FCT_business_DIM_gen_period" FOREIGN KEY (gen_period_ID )REFERENCES dim_gen_period (gen_period_ID);
+   
+ALTER TABLE FCT_business
+   ADD CONSTRAINT "FK_FCT_business_DIM_product" FOREIGN KEY (product_ID )REFERENCES dim_product (product_ID);
+   
+ALTER TABLE FCT_business
+   ADD CONSTRAINT "FK_FCT_business_DIM_promotion" FOREIGN KEY (promotion_ID)REFERENCES dim_promotion (promotion_ID);
+   
+
+select * from FCT_business order by 1;
+
+SELECT * FROM ALL_TAB_PARTITIONS;
+select * from ALL_PART_TABLES where table_name = 'FCT_business';
+
+--The below query gives the number of rows in SUBPARTITION
+select num_rows, PARTITION_NAME , SUBPARTITION_NAME 
+FROM ALL_TAB_SUBPARTITIONS
+where table_name = 'FCT_business';
+
+--The below query gives the number of rows in PARTITION and also the number of sub partitions.
+select num_rows, PARTITION_NAME, SUBPARTITION_COUNT     
+FROM ALL_TAB_SUBPARTITIONS
+where table_name = 'FCT_business';
+
+select * from FCT_business where promotion_id=219504;
+select count(*) from FCT_business;
+   
+   
