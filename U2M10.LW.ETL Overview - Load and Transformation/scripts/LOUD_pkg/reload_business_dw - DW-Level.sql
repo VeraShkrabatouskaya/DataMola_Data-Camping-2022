4d964fa0442c41f77be059975599f94961003a16
VeraShkrabatouskaya <98982851+VeraShkrabatouskaya@users.noreply.github.com>
2022-08-21 13:01:20 +0300

    Create reload_business_dw - DW-Level.sql

---
+++ U2M10.LW.ETL Overview - Load and Transformation/scripts/LOUD_pkg/reload_business_dw - DW-Level.sql
@@ -0,0 +1,21 @@
+alter session set current_schema = DW_DATA;
+alter user DW_DATA QUOTA UNLIMITED ON TS_DW_DATA_01;
+
+BEGIN
+   pkg_etl_dw_customer.load_dw_customer;
+   pkg_etl_dw_employee.load_dw_employee;
+   pkg_etl_dw_agency.load_dw_agency;
+   pkg_etl_dw_gen_period.load_dw_gen_period;
+   pkg_etl_dw_product.load_dw_product;
+   pkg_etl_dw_promotion.load_dw_promotion;
+   pkg_etl_dw_transaction.load_dw_transaction;
+END;
+
+SELECT * FROM DW_DATA.DIM_customer;
+SELECT * FROM DW_DATA.DIM_employee;
+SELECT * FROM DW_DATA.DIM_agency;
+SELECT * FROM DW_DATA.DIM_gen_period;
+SELECT * FROM DW_DATA.DIM_product;
+SELECT * FROM DW_DATA.DIM_promotion;
+SELECT * from DW_DATA.FCT_business;
+ 
