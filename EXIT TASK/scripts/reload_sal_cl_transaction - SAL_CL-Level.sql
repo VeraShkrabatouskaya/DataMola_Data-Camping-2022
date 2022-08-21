2adf464dcc0a5a7cbb24e8ac7d410440790163bb
VeraShkrabatouskaya <98982851+VeraShkrabatouskaya@users.noreply.github.com>
2022-08-22 02:33:53 +0300

    Delete reload_sal_cl_transaction - SAL_CL-Level.sql

--- EXIT TASK/scripts/reload_sal_cl_transaction - SAL_CL-Level.sql
+++
@@ -1,9 +0,0 @@
-alter session set current_schema = SAL_CL;
-GRANT SELECT ON SAL_DW_CL.sal_dw_cl_t_transaction TO SAL_CL;
-
-BEGIN
-   pkg_etl_sal_cl_t_transaction.load_sal_cl_t_transaction;
-END;
-
-SELECT * FROM sal_cl_t_transaction order by 1;
- 
