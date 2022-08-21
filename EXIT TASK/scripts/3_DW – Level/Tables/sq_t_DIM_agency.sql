477c86d21cd62f2b686e18507f7de604d7b68357
VeraShkrabatouskaya <98982851+VeraShkrabatouskaya@users.noreply.github.com>
2022-08-22 02:27:23 +0300

    Create sq_t_DIM_agency.sql

---
+++ EXIT TASK/scripts/3_DW – Level/Tables/sq_t_DIM_agency.sql
@@ -0,0 +1,8 @@
+--drop sequence DW_DATA.SQ_DIM_agency;
+--alter session set current_schema = DW_DATA;
+
+CREATE SEQUENCE DW_DATA.SQ_DIM_agency
+    START WITH        1    
+    INCREMENT BY      1
+    NOCACHE
+    NOCYCLE;
