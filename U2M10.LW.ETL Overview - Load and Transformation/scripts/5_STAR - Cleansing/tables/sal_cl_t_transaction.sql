cf92ecad7e99c39571298344e47748ea2c842d04
VeraShkrabatouskaya <98982851+VeraShkrabatouskaya@users.noreply.github.com>
2022-08-21 14:32:34 +0300

    Add files via upload

--- U2M10.LW.ETL Overview - Load and Transformation/scripts/5_STAR - Cleansing/tables/sal_cl_t_transaction.sql
+++ U2M10.LW.ETL Overview - Load and Transformation/scripts/5_STAR - Cleansing/tables/sal_cl_t_transaction.sql
@@ -1,5 +1,5 @@
 alter session set current_schema=SAL_CL;
---drop table sal_cl_t_transaction CASCADE CONSTRAINTS;
+drop table sal_cl_t_transaction CASCADE CONSTRAINTS;
 
 alter session set current_schema=sal_dw_cl;
 GRANT SELECT ON sal_dw_cl.sal_dw_cl_t_transaction TO SAL_CL;
@@ -48,7 +48,28 @@ Create table sal_cl_t_transaction (
     net_salary_employee_dollar_amount DECIMAL (10,2) NOT NULL,
     gross_profit_margin_percent         DECIMAL (10,2) NOT NULL,
     CONSTRAINT "PK_T.sal_cl_t_transaction" PRIMARY KEY(transaction_ID)
+)
+PARTITION BY RANGE (TIME_ID)
+(
+    PARTITION QUARTER_1 VALUES LESS THAN(TO_DATE('01/04/2022', 'DD/MM/YYYY'))
+,
+    PARTITION QUARTER_2 VALUES LESS THAN(TO_DATE('01/07/2022', 'DD/MM/YYYY'))
+,
+     PARTITION QUARTER_3 VALUES LESS THAN(TO_DATE('01/10/2022', 'DD/MM/YYYY'))
+,
+     PARTITION QUARTER_4 VALUES LESS THAN(TO_DATE('01/01/2023', 'DD/MM/YYYY'))
 );
 
 alter session set current_schema=SAL_CL;
 select * from sal_cl.sal_cl_t_transaction order by 1;
+
+SELECT * FROM ALL_TAB_PARTITIONS;
+SELECT * FROM ALL_PART_TABLES WHERE TABLE_NAME = 'SAL_CL_T_TRANSACTION';
+
+select num_rows, PARTITION_NAME , SUBPARTITION_NAME 
+FROM ALL_TAB_SUBPARTITIONS;
+
+SELECT * FROM SAL_CL_T_TRANSACTION partition (QUARTER_1);
+SELECT * FROM SAL_CL_T_TRANSACTION partition (QUARTER_2);
+SELECT * FROM SAL_CL_T_TRANSACTION partition (QUARTER_3);
+SELECT * FROM SAL_CL_T_TRANSACTION partition (QUARTER_4);   
